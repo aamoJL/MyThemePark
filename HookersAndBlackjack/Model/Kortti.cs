112c1c14f1c9a9cb9734aa439258b4f6dc85c053
K1729 <K1729@student.jamk.fi>
2016-03-25 21:11:53 +0200

    I don't ask why developers commit, I just assume they all do.

--- HookersAndBlackjack/Model/Kortti.cs
+++ HookersAndBlackjack/Model/Kortti.cs
@@ -4,12 +4,13 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using System.Diagnostics;
 namespace HookersAndBlackjack.Model
 {
     class Kortti
     {
-        public string Suit;
-        public ushort Number;
+        public string Suit { get; }
+        public ushort Number { get; }
 
         public Kortti() { }
 
@@ -17,6 +18,11 @@ namespace HookersAndBlackjack.Model
         {
             Suit = suit;
             Number = number;
+        }
+
+        ~Kortti()
+        {
+            Debug.WriteLine("Object removed.");
         }
 
         public override string ToString()
