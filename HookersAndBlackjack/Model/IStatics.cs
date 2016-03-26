40eaa0cffbe5f43b4a8136bf818d4d7febe6d4f4
aamoJL <jonilaukka@hotmail.com>
2016-03-27 01:18:57 +0200

    human, statics, player

---
+++ HookersAndBlackjack/Model/IStatics.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HookersAndBlackjack.Model
+{
+    interface IStatics
+    {
+        int Money { get; set; }
+        int Chips { get; set; }
+        int Loses { get; set; }
+        int Games { get; set; }
+        int Loans { get; set; }
+    }
+}
