67a858ebebedeb95861c920aacf755cf3dc261b2
K1729 <K1729@student.jamk.fi>
2016-03-26 02:41:16 +0200

    The commit is out there.

--- HookersAndBlackjack/Blackjack.xaml.cs
+++ HookersAndBlackjack/Blackjack.xaml.cs
@@ -23,6 +23,7 @@ namespace HookersAndBlackjack
     /// </summary>
     public sealed partial class Blackjack : Page
     {
+        private Table House = new Table();
         public Blackjack()
         {
             this.InitializeComponent();
@@ -31,80 +32,18 @@ namespace HookersAndBlackjack
         private void Deal_Click(object sender, RoutedEventArgs e)
         {
             DebugScreen.Text = "";
-            ushort n;
-            Random rand = new Random();
+            House.Deal(1);
+        }
 
-            //Jokainen maa on oma lista
-            List<Kortti> Spades = new List<Kortti>();
-            List<Kortti> Hearts = new List<Kortti>();
-            List<Kortti> Diamonds = new List<Kortti>();
-            List<Kortti> Clubs = new List<Kortti>();
-            //Pakka on myös lista, johon myöhemmin muut listat lisätään.
-            List<Kortti> Pack = new List<Kortti>();
-            //Käsi
-            List<Kortti> Hand = new List<Kortti>();
-            //Pöytä
-            List<Kortti> House = new List<Kortti>();
-            //Checker
-            List<Kortti> Cheker = new List<Kortti>();
-            //Ylimääräinen kortti. Sekoitusta varten.
-            Kortti T = new Kortti();
-
-            //Spades
-            for (ushort i = 0; i < 13; i++)
+        private void DebugButton_Click(object sender, RoutedEventArgs e)
+        {
+            try
             {
-                Kortti kortti = new Kortti("♠", (ushort)(i + 1));
-                Spades.Add(kortti);
+                DebugScreen.Text = House.DebugMessage;
             }
-            //Hertta
-            for (ushort i = 0; i < 13; i++)
+            catch
             {
-                Kortti kortti = new Kortti("♥", (ushort)(i + 1));
-                Hearts.Add(kortti);
-            }
-            //Diamonds
-            for (ushort i = 0; i < 13; i++)
-            {
-                Kortti kortti = new Kortti("♦", (ushort)(i + 1));
-                Diamonds.Add(kortti);
-            }
-            //Clubs
-            for (ushort i = 0; i < 13; i++)
-            {
-                Kortti kortti = new Kortti("♣", (ushort)(i + 1));
-                Clubs.Add(kortti);
-            }
-
-            //Lisätään kaikki maat pakkaan.
-            Pack.AddRange(Spades);
-            Pack.AddRange(Hearts);
-            Pack.AddRange(Diamonds);
-            Pack.AddRange(Clubs);
-
-            //Fisher-Yates sekoitus
-            n = (ushort)Pack.Count;
-            while (n > 1)
-            {
-                n--;
-                int k = rand.Next(n + 1);
-                T = Pack[k];
-                Pack[k] = Pack[n];
-                Pack[n] = T;
-            }
-            ushort x = (ushort)Pack.Count;
-            for (n = 0; n < 2; n++)
-            {
-                x--;
-                Hand.Add(Pack[x]);
-                Pack.RemoveAt(x);
-            }
-
-            DebugScreen.Text += "Pack count: " + Pack.Count + "\n";
-            DebugScreen.Text += "Hand count: " + Hand.Count + "\n";
-
-            foreach (Kortti k in Hand)
-            {
-                DebugScreen.Text += k.ToString();
+                DebugScreen.Text = "No Data";
             }
         }
     }
